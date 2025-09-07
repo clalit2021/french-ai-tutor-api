639ee97c9f9531673735cbacb105806c6ecf9731
clalit2021 <83096459+clalit2021@users.noreply.github.com>
2025-09-07 23:21:48 +0300

    Fix string quotes in celery_app.py configuration

--- app/celery_app.py
+++ app/celery_app.py
@@ -1,3 +1,4 @@
+# app/celery_app.py
 import os
 from celery import Celery
 
@@ -9,13 +10,17 @@ celery_app = Celery(
     broker=BROKER,
     backend=BACKEND,
 )
+
 celery_app.conf.update(
-    task_serializer='json',
-    accept_content=['json'],
-    result_serializer='json',
-    timezone='UTC',
+    task_serializer="json",
+    accept_content=["json"],
+    result_serializer="json",
+    timezone="UTC",
     enable_utc=True,
+    broker_connection_retry_on_startup=True,
+    task_ignore_result=True,          # set False if you need results
+    worker_hijack_root_logger=False,  # let your app control logging
 )
 
 # Autodiscover tasks in the "app" package
-celery_app.autodiscover_tasks(['app'])
+celery_app.autodiscover_tasks(["app"])
